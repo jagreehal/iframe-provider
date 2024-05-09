dabd18d80feff71fffc69cdcdc81a30e99e31d5c
Jag Reehal <jag.reehal@gmail.com>
2024-05-09 14:38:31 +0100

    csp

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const cspHeader = ` 
+    frame-ancestors *;
+`;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
+};
